54dfd03b64d64be5898fe46b19d521257aecb6bd
Cainnan Carra <league@WTS10.attlocal.net>
2018-04-08 15:54:55 -0700

    shrek shrek and ahrek

---
+++ src/shrek.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class shrek {
+public static void main(String[] args) {
+	 String name=JOptionPane.showInputDialog ("whats your name?") ;
+	 JOptionPane.showMessageDialog(null,"hi "+name);
+}
+}
---
+++ src/shrek2.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class shrek2 {
+public static void main(String[] args) {
+	String name= JOptionPane.showInputDialog ("whats your name?") ;
+	JOptionPane.showMessageDialog(null,"i know what you did last summer");
+}
+}
