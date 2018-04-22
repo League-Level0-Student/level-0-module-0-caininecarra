a21609afaa2081e69580da6cf2426b29cb0659dd
Cainnan Carra <league@WTS-IM-6.attlocal.net>
2018-04-22 15:28:39 -0700

    I know what you did last summer

--- src/shrek2.java
+++ src/shrek2.java
@@ -3,6 +3,6 @@ import javax.swing.JOptionPane;
 public class shrek2 {
 public static void main(String[] args) {
 	String name= JOptionPane.showInputDialog ("whats your name?") ;
-	JOptionPane.showMessageDialog(null,"i know what you did last summer");
+	JOptionPane.showMessageDialog(null,"i know what you did last summer "+name);
 }
 }
